eae00bd17dfe6eeab6d475af857d7a79ff067932
userbaj <64317950+userbaj@users.noreply.github.com>
2022-12-02 16:33:23 +0545

    added: credential form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,31 @@ function App() {
   return (
     <div className="App">
      <h2>Silverline System Pvt Ltd</h2>
+     <small>Add your credentials</small>
+     <form>
+      <div className="add-details">
+        <div className="full-name">
+          <label>Full Name:</label>
+          <input type="text"/>
+        </div>
+        <div className="address">
+          <label>Address:</label>
+          <input type="text"/>
+        </div>
+        <div className="pan-num">
+          <label>PAN number:</label>
+          <input type="text"/>
+        </div>
+        <div className="email">
+          <label>Email:</label>
+          <input type="text"/>
+        </div>
+        <div className="phone-num">
+          <label>Phone number:</label>
+          <input type="text"/>
+        </div>
+      </div>
+     </form>
     </div>
   );
 }
